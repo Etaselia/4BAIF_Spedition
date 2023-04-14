9207be006ee931a2dabc8baa98061112922d4398
Simon Ptak <progamersp2@gmail.com>
2023-04-14 15:47:31 +0200

    Fixed Unit Conversions

--- src/main/java/net/eta/SpeditionComparatorBuildYear.java
+++ src/main/java/net/eta/SpeditionComparatorBuildYear.java
@@ -5,9 +5,6 @@ import java.util.Comparator;
 public class SpeditionComparatorBuildYear implements Comparator<Vehicles> {
     @Override
     public int compare(Vehicles o1, Vehicles o2) {
-        if(o1.getBuildYear().isBefore(o2.getBuildYear())){
-            return 1;
-        }
-        return -1;
+        return o1.getBuildYear().compareTo(o2.getBuildYear());
     }
 }
