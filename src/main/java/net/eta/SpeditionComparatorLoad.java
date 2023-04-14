0d4519e576d42ff7be458d99a03c967606571a1b
Simon Ptak <progamersp2@gmail.com>
2023-04-14 15:14:41 +0200

    Fixed Unit Conversions

--- src/main/java/net/eta/SpeditionComparatorLoad.java
+++ src/main/java/net/eta/SpeditionComparatorLoad.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class SpeditionComparatorLoad implements Comparator<Vehicles> {
     @Override
     public int compare(Vehicles o1, Vehicles o2) {
-        return Integer.compare(o1.getLoad(),o2.getLoad());
+        return Integer.compare(o2.getLoad(),o1.getLoad());
     }
 }
